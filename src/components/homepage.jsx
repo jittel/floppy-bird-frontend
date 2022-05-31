4e80bd950e427093cc701a6d1adda91a8267b176
SFelbeck <stefanfelbeck@gmail.com>
2022-05-31 14:15:02 -0700

    more intro pages

---
+++ src/components/homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import { motion } from "framer-motion";
+
+
+export default function chickenpage(){
+    // <motion.div animate={{ x: 100 }}>
+    //     <img src="./public/yicken.png" alt="yicken"></img>
+    // </motion.div>
+    <div>
+    <img src="../../public/yicken.png" alt="yicken"></img>
+    </div>
+}
