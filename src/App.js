030c206df6ab8aaa8aa9309129a9cbae0aac9d5d
jittel <nathan.c.ginn@gmail.com>
2022-06-01 11:33:32 -0700

    fixed homepage and login routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
-import IntroPage from "./components/IntroPage";
-// import Login from "./components/login";
+import Login from "./components/login";
+import Homepage from "./components/Homepage";
+// import IntroPage from "./components/IntroPage";
 // import Signup from "./components/signup";
-// import Homepage from "./components/homepage";
+// import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-function App(){
-  return(
-    <div>
-      <IntroPage />
-      {/* <Signup /> */}
-      {/* <Login /> */}
-      {/* <Homepage /> */}
-    </div>
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+    // <Homepage />
   )
 }
 
