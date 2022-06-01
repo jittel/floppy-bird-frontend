ffa0e2332698f97232b135f9f09a51c614955f8c
SFelbeck <stefanfelbeck@gmail.com>
2022-06-01 13:43:17 -0700

    components

---
+++ src/components/ShoesSubtype/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// note there is currently no back end to plug in here so most of this shall remain blank
+export default function Shoes() {
+    return(
+        <div className='accCard' id='storeCard'>
+            <ul id='shoeslist'>
+                {/* tbd */}
+            </ul>
+        </div>
+    )
+}
