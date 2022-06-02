b11462ac20036b457a36c85aa2a7aaafaf34ffa2
SFelbeck <stefanfelbeck@gmail.com>
2022-06-02 11:52:35 -0700

    looping bounce animation

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,16 +1,11 @@
 import React from "react";
 import { motion } from "framer-motion";
-// import yicken from "../assets/yicken.png"
 
 
 export default function Homepage(){
    return(
-    <motion.div animate={{ x: 100 }}>
+    <motion.div animate={{ y: 100 }} transition={{ yoyo: Infinity }}>
         <img src={require("../assets/yicken.png")} alt="yicken"></img>
     </motion.div>
-    // <div>
-    //     <h1>hello world</h1>
-    // <img src="./yicken.png" alt="yicken"></img>
-    // </div>
    )
 }
