b653377f960704b8fa338fb0a1443c1ee8bc1875
SFelbeck <stefanfelbeck@gmail.com>
2022-06-02 13:35:49 -0700

    burger slide semi-functionality

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,11 +1,23 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { motion, useDragControls } from "framer-motion";
 
 
 export default function Homepage(){
+    const dragControls = useDragControls();
+
+    function startDrag(event) {
+        dragControls.start(event, { snapToCursor: false })
+      }
+
    return(
-    <motion.div animate={{ y: 100 }} transition={{ yoyo: Infinity }}>
-        <img src={require("../assets/yicken.png")} alt="yicken"></img>
-    </motion.div>
+    <div>
+        <motion.div animate={{ y: 100 }} transition={{ yoyo: Infinity }}>
+            <img src={require("../assets/yicken.png")} alt="yicken"></img>
+        </motion.div>
+        <motion.div onPointerDown={startDrag} drag="x" dragControls={dragControls}>
+            <img src={require("../assets/borgor.png")} alt="burger stock"></img>
+        </motion.div>
+    </div>
+    
    )
 }
