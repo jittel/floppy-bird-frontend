e5a3fa5b08d43a354c40ba17175d34c7fd3d410d
Lola Applegate <laurenemilya@gmail.com>
2022-06-03 11:24:56 -0700

    added link to app.js & imported items for storefront drawer

--- src/components/Store/index.js
+++ src/components/Store/index.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 
 export default function Store() {
     return(
