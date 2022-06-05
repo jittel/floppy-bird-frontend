760189951aa324dba0c8fd30d13d31aa1377019e
Jeff Hicks <hicks.jeff@me.com>
2022-06-05 15:25:50 -0700

    added image pngs and linked to homepage

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,20 +1,37 @@
-import React from "react";
-import { motion, useDragControls } from "framer-motion";
+import React, { useRef } from "react";
+import { motion } from "framer-motion";
+import arms from "../assets/arms/index.js";
+import hats from "../assets/hats/index.js";
+import shoes from "../assets/shoes/index.js";
+import './Style.css'
 
 
-export default function Homepage(){
+export default function Homepage() {
+    const windowdim = useRef(null);
+    function changeHat() {
+        var armEl = document.getElementById('arm')
+        armEl.src=arms.basketball
+    }
+    return (
+        <div style={{ width: "100vw", height: "100vh", overflow: "hidden" }} ref={windowdim}>
+                <button className="changeHat" onClick={changeHat}>Change Arms</button>
+            {/* <motion.div animate={{ y: 100 }} transition={{ yoyo: Infinity }}> */}
+            <motion.div>
+                <img src={require("../assets/floppy-bird.png")} alt="yicken" className="chicken"></img>
+                <motion.img drag
+                    dragConstraints={windowdim}
+                    src={require("../assets/hat/hat_cap.png")} alt="burger stock" className="borgor" id="hat">
+                </motion.img>
+                <motion.img drag
+                    dragConstraints={windowdim}
+                    src={require("../assets/shoes/shoes_boots_with_the_fur.png")} alt="burger stock" className="borgor" id="shoe">
+                </motion.img>
+                <motion.img drag
+                    dragConstraints={windowdim}
+                    src={require("../assets/arm/arms_yas.png")} alt="burger stock" className="borgor" id="arm">
+                </motion.img>
+            </motion.div>
+        </div>
 
-   return(
-    <div>
-        <motion.div animate={{ y: 100 }} transition={{ yoyo: Infinity }}>
-            <img src={require("../assets/yicken.png")} alt="yicken"></img>
-        </motion.div>
-        <motion.div drag 
-        dragConstraints={{ left: 0, top: 500, right: 2000, bottom: 500 }}
-        >
-            <img src={require("../assets/borgor.png")} alt="burger stock"></img>
-        </motion.div>
-    </div>
-    
-   )
+    )
 }
---
+++ src/components/assets/shoes/index.js
@@ -0,0 +1,10 @@
+const shoes = {
+    boots_with_the_fur: require('./shoes_boots_with_the_fur.png'),
+    cowboy_boots: require('./shoes_cowboy_boots.png'),
+    crocs: require('./shoes_crocs.png'),
+    basketball_shoes: require('./shoes_basketball_shoes.png'),
+    platform: require('./shoes_platforms.png'),
+    slides: require('./shoes_slides.png')
+}
+
+export default shoes
