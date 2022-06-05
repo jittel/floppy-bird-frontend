b12dfed5e13d515999942f6fb863e35077151651
SFelbeck <stefanfelbeck@gmail.com>
2022-06-05 16:01:32 -0700

    basic egg incrementation

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import borgor from "../assets/borgor.png";
 import './Style.css'
@@ -6,6 +6,10 @@ import './Style.css'
 
 export default function Homepage(){
     const windowdim = useRef(null);
+    const [eggCount, setCount] = useState(0);
+        setInterval(() =>{
+            setCount(eggCount + 1);
+        }, 5000)
    return(
     <div style={{width: "100vw", height: "100vh", overflow: "hidden"}} ref={windowdim}>
         <motion.div animate={{ y: 100 }} transition={{ yoyo: Infinity }}>
@@ -14,7 +18,8 @@ export default function Homepage(){
             <motion.img drag 
         dragConstraints={windowdim}
          src={borgor} alt="burger stock" className="borgor"></motion.img>
+        <p className="egg-counter">Number of eggs: {eggCount}</p>
     </div>
-    
    )
 }
+
