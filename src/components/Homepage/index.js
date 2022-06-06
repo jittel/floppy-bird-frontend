174c1f4bedf875ce7f7bada329b074ca697f4404
Jeff Hicks <hicks.jeff@me.com>
2022-06-06 10:09:10 -0700

    started scripts

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -16,19 +16,19 @@ export default function Homepage() {
         <div style={{ width: "100vw", height: "100vh", overflow: "hidden" }} ref={windowdim}>
                 <button className="changeHat" onClick={changeHat}>Change Arms</button>
             {/* <motion.div animate={{ y: 100 }} transition={{ yoyo: Infinity }}> */}
-            <motion.div>
+            <motion.div className="imageContainer">
                 <img src={require("../assets/floppy-bird.png")} alt="yicken" className="chicken"></img>
                 <motion.img drag
                     dragConstraints={windowdim}
-                    src={hats.crown} alt="burger stock" className="borgor" id="hat">
+                    src={hats.crown} alt="burger stock" className="accessory" id="hat">
                 </motion.img>
                 <motion.img drag
                     dragConstraints={windowdim}
-                    src={shoes.basketball_shoes} alt="burger stock" className="borgor" id="shoe">
+                    src={shoes.basketball_shoes} alt="burger stock" className="accessory" id="shoe">
                 </motion.img>
                 <motion.img drag
                     dragConstraints={windowdim}
-                    src={arms.basketball} alt="burger stock" className="borgor" id="arm">
+                    src={arms.basketball} alt="burger stock" className="accessory" id="arm">
                 </motion.img>
             </motion.div>
         </div>
