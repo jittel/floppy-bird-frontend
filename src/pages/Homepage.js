b6077e9f1c486030c18ba3ed43ce42afd939b8b8
jittel <nathan.c.ginn@gmail.com>
2022-06-06 14:44:37 -0700

    logout works

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Store from '../components/Store';
 import Homepage from '../components/Homepage';
+import Logout from '../components/Logout';
 
-export default function Pages() {
+export default function Home(props) {
     return (
         <div>
             <Store />
+            <Logout logout={props.logout} />
             <Homepage />
         </div>
     )
