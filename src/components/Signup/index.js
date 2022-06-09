51e083d25169a717a1743409f4e0d5493823a371
Lola Applegate <laurenemilya@gmail.com>
2022-06-09 15:57:21 -0700

    intro, login & signup pages styled

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import './Style.css'
 import API from "../../utils/API"
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { FormControl } from '@mui/material';
 
 export default function Signup() {
 
@@ -32,16 +35,79 @@ export default function Signup() {
       };
 
     return (
-        <div>
+        <Box
+            component="form"
+            onSubmit={handleFormSubmit}
+            className='customFormBox'
+        >
+            <FormControl>
+                <TextField 
+                    className='custom-input'
+                    variant='outlined'
+                    color='secondary'
+                    id='username'
+                    name="username"
+                    label="username"
+                    type="text"
+                    onChange={handleInputChange}
+                ></TextField>
+            </FormControl>
+            <br></br>
+            <FormControl>
+                <TextField 
+                    className='custom-input'
+                    variant='outlined'
+                    color='secondary'
+                    id='password'
+                    name="password"
+                    label="password"
+                    type="text"
+                    onChange={handleInputChange}
+                ></TextField>
+            </FormControl>
+            <br></br>
+            <FormControl>
+                <TextField 
+                    className='custom-input'
+                    variant='outlined'
+                    color='secondary'
+                    id='passwordConfirm'
+                    // name="password"
+                    label="confirm password"
+                    type="text"
+                    onChange={handleInputChange}
+                ></TextField>
+            </FormControl>
+            <br></br>
+            <FormControl>
+                <TextField 
+                    className='custom-input'
+                    variant='outlined'
+                    color='secondary'
+                    id='chickenName'
+                    name="chicken_name"
+                    label="name your chicken!"
+                    type="text"
+                    onChange={handleInputChange}
+                ></TextField>
+            </FormControl>
+            <br></br>
+            <button className='submitButton' href="/" >Submit</button>
+        </Box>
+    )
+}
+
+{/* <div>
             <form onSubmit={handleFormSubmit}>
                 <section className='pagecard'>
                     <input id='username' name='username' type='text' placeholder='Username' onChange={handleInputChange}></input>
+
                     <input id='password' name='password' type='text' placeholder='Password' onChange={handleInputChange}></input>
+
                     <input id='passwordConfirm' type='text' placeholder='Confirm Password' onChange={handleInputChange}></input>
+
                     <input id='chickenName' name='chicken_name' type='text' placeholder='Name Your chicken!' onChange={handleInputChange}></input>
                     <button className='submitButton' href="/">Submit</button>
                 </section>
             </form>
-        </div>
-    )
-}
+        </div> */}
